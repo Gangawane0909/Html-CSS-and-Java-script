6063f78886f4b7c7af6db7201e0cfdb6d233fee1
Gangawane0909 <sgangawane1234@gmail.com>
2022-04-09 10:32:21 +0530

    abcd

---
+++ javascript/javascript-intro.js
@@ -0,0 +1,40 @@
+// alert("Hello, welcome to javascript")
+
+//let,const and var keywords
+
+//global scope, local/block scope and function scope
+
+//basic programming concepts- variables, functions, data types, conditional statments, iteration statments
+
+//hoisting in javascript
+
+
+//declaration
+//initialization
+//using variable/function
+
+//var keyword variable is a global scope variable as well as function
+//let keyword variable is a block scope variable as function scope
+//const keyword variable
+
+const PI = 3.14;
+PI = 3.14;
+
+console.log(PI)
+var num1;
+
+num1 = 200;
+num1 = 500;
+console.log(num1)
+if(true){
+    var number = 100
+}
+
+console.log(number)
+function displayMessage(){
+    var fname = "codekul"
+}
+
+displayMessage()
+
+console.log("welcome to codekul")
